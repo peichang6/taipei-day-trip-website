6b8574b856776b2fe3bd00f9a35659beec81d096
pei <ya7907ya@gmail.com>
2022-03-09 17:46:49 +0800

    1st update

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 from flask import *
+from mysql.connector import pooling
+import mysql.connector
+from attractions_api import attractions
+
 app = Flask(__name__)
+
+app.register_blueprint(attractions)
+
+app.secret_key = "the key"
 app.config["JSON_AS_ASCII"] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['JSON_SORT_KEYS'] = False
 
-# Pages
 
-
-@app.route("/")
+@ app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/attraction/<id>")
+@ app.route("/attraction/<id>")
 def attraction(id):
     return render_template("attraction.html")
 
 
-@app.route("/booking")
+@ app.route("/booking")
 def booking():
     return render_template("booking.html")
 
 
-@app.route("/thankyou")
+@ app.route("/thankyou")
 def thankyou():
     return render_template("thankyou.html")
 
 
-app.run(port=3000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000, debug=True)
